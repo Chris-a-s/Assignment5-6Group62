e0bbabd355d97d458626001584587447e7d9d75a
cminkus <cminkus@asu.edu>
2025-04-14 12:40:42 -0700

    Initial commit with .gitignore to exclude temp files

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="HoopHub - Player Service" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment5_CSE445_Group_62._Default" %>
+<%@ Register TagPrefix="uc" TagName="PlayerStatsPanel" Src="~/UserControls/PlayerStatsPanel.ascx" %>
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>TryIt: Get Player Average Points</h1>
+    <asp:TextBox ID="txtPlayerAvg" runat="server" />
+    <asp:Button ID="btnAvg" runat="server" Text="Get Avg PPG" OnClick="btnAvg_Click" />
+    <asp:Label ID="lblAvg" runat="server" /><br /><br />
+
+    <h2>TryIt: Get Total Games Played</h2>
+    <asp:TextBox ID="txtPlayerGames" runat="server" />
+    <asp:Button ID="btnGames" runat="server" Text="Get Total Games" OnClick="btnGames_Click" />
+    <asp:Label ID="lblGames" runat="server" /><br /><br />
+
+    <h3>TryIt: Get Player Stats Summary</h3>
+    <asp:TextBox ID="txtPlayerSummary" runat="server" />
+    <asp:Button ID="btnSummary" runat="server" Text="Get Summary" OnClick="btnSummary_Click" />
+    <asp:Label ID="lblSummary" runat="server" /> 
+    <uc:PlayerStatsPanel ID="PlayerStatsPanel1" runat="server" />
+
+    <h4>Session Info:</h4>
+    <asp:Label ID="lblSession" runat="server" />
+
+
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using Assignment5_CSE445_Group_62.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment5_CSE445_Group_62
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Simulated login
+                Session["username"] = "jason";
+                Session["role"] = "member";
+            }
+
+            lblSession.Text = "Logged in as: " + Session["username"] + " (Role: " + Session["role"] + ")";
+
+        }
+
+
+        protected void btnAvg_Click(object sender, EventArgs e)
+        {
+            PlayerService svc = new PlayerService();
+            double avg = svc.GetPlayerAveragePoints(txtPlayerAvg.Text);
+            lblAvg.Text = "Avg PPG: " + avg.ToString("F1");
+        }
+
+        protected void btnGames_Click(object sender, EventArgs e)
+        {
+            PlayerService svc = new PlayerService();
+            int total = svc.GetTotalGamesPlayed(txtPlayerGames.Text);
+            lblGames.Text = "Games Played: " + total.ToString();
+        }
+
+        protected void btnSummary_Click(object sender, EventArgs e)
+        {
+            PlayerService svc = new PlayerService();
+            string summary = svc.GetPlayerStatsSummary(txtPlayerSummary.Text);
+            lblSummary.Text = summary;
+        }
+
+    }
+}
